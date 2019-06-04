a6b73f3d8898a7c858d711925ea7b875aa15a1a7
Roberto E. B. Junior <robertoeb.jr@gmail.com>
2019-06-03 22:58:02 -0300

    Add side nav bar and add active effect on categories buttons

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
-import { Container, Navbar, NavItem, SearchForm, Row } from "react-materialize";
+import { Container, Row, SideNavItem, Navbar } from "react-materialize";
 
 import "./styles.css";
 import logoMini from "../../assets/img/logo-mini.svg";
+import userImage from "../../assets/img/user.png";
+import sideNavBackground from "../../assets/img/background.jpg";
 
 export default class Main extends Component {
-  onClick = () => {};
+  setActive = event => {
+    document.querySelector(".active").classList.remove("active");
+    event.target.classList.add("active");
+  };
 
   render() {
     return (
@@ -14,13 +19,34 @@ export default class Main extends Component {
         <Navbar
           className="main-nav"
           brand={<img className="logo" src={logoMini} alt="Accountant Logo" />}
-          alignLinks="right"
+          centerLogo
+          alignLinks="left"
           fixed
         >
-          <NavItem onClick={this.onClick}>Getting started</NavItem>
-          <NavItem href="components.html">Components</NavItem>
+          <div id="side-nav">
+            <SideNavItem
+              userView
+              user={{
+                background: sideNavBackground,
+                image: userImage,
+                name: "John Doe",
+                email: "johndoe@mail.com"
+              }}
+            />
+            <SideNavItem href="#!icon" icon="cloud">
+              First Link With Icon
+            </SideNavItem>
+            <SideNavItem href="#!second">Second Link</SideNavItem>
+            <SideNavItem divider />
+            <SideNavItem subheader>Subheader</SideNavItem>
+            <SideNavItem waves href="#!third">
+              Third Link With Waves
+            </SideNavItem>
+          </div>
         </Navbar>
-        <SearchForm />
+
+        <Navbar className="main-nav-search" search />
+
         <Container>
           <Row>
             <h1>Pesquisar por categoria</h1>
@@ -28,14 +54,14 @@ export default class Main extends Component {
         </Container>
         <Row className="categories-row">
           <div className="categories">
-            <div className="categories-item" active>
-              <div className="categories-item-content">Imposto de renda</div>
+            <div className="categories-item active" onClick={this.setActive}>
+              <span>Imposto de renda</span>
             </div>
-            <div className="categories-item">
-              <div className="categories-item-content">Abrir empresa</div>
+            <div className="categories-item" onClick={this.setActive}>
+              <span>Abrir empresa</span>
             </div>
-            <div className="categories-item">
-              <div className="categories-item-content">Consultoria</div>
+            <div className="categories-item" onClick={this.setActive}>
+              <span>Consultoria</span>
             </div>
           </div>
         </Row>
