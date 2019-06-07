ec07bcf3a47d4f5555a74d596bd5935a1bac3e32
Roberto E. B. Junior <robertoeb.jr@gmail.com>
2019-06-06 21:50:28 -0300

    Fix routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Main from "./pages/Main";
 import Jobs from "./pages/Jobs";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 
 const Routes = () => (
   <BrowserRouter>
@@ -12,7 +11,6 @@ const Routes = () => (
       <Route path="/" exact component={Main} />
       <Route path="/jobs" exact component={Jobs} />
       <Route path="/login" exact component={Login} />
-      <Route path="/register" exact component={Register} />
     </Switch>
   </BrowserRouter>
 );
